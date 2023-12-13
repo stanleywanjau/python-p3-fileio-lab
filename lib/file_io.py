02feedb70a7a2203cde0810d86c38afda2fc558c
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-13 21:56:53 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
+from pathlib import Path
 def write_file(file_name, file_content):
-    pass
+     file_path=f"{file_name}.txt"
+     with open(file_path,mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+
+    
 
 def append_file(file_name, append_content):
-    pass
+     file_path=f"{file_name}.txt"
+     with open(file_path, 'a') as file:
+        file.write(append_content)
+
+pass
 
 def read_file(file_name):
+    file_path=f"{file_name}.txt"
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
     pass
